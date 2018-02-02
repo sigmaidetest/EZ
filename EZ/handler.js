55f2e9584f5d93d710b864bfbb6e9832c13fe808
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-02-02 13:00:47 +0530

    updating header comment

--- EZ/handler.js
+++ EZ/handler.js
@@ -1,4 +1,7 @@
-// Adapted from https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example-deployment-pkg.html
+/*
+ * Original source: https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example-deployment-pkg.html
+ * Retrieved on:    2018-01-19
+ */
 
 // Dependencies
 let AWS = require('aws-sdk');
